ed97ad9a15b24e44af60ecf010bfab14f0e571d8
Shubham Jangle <jangleshubham13@gmail.com>
2023-11-05 14:03:33 +0530

    feat: Added products hero section

--- app/(client)/_components/products/HeroSectionForCategory.tsx
+++ app/(client)/_components/products/HeroSectionForCategory.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+
 const HeroSectionForCategory = () => {
-  return <div>HeroSectionForCategory</div>;
+  return (
+    <div className="space-y-2">
+      <div className="flex w-full rounded-lg overflow-clip h-fit">
+        <Image
+          alt={""}
+          src={"/products_cover_1.png"}
+          width={1200}
+          height={300}
+          className="mx-auto"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default HeroSectionForCategory;
