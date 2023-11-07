45c379fcf3563ff959960d26d0b6a74854f954d2
Shubham Jangle <jangleshubham13@gmail.com>
2023-11-07 15:19:52 +0530

    feat: Improved filter

--- app/(client)/_components/products/Filters.tsx
+++ app/(client)/_components/products/Filters.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Select,
   SelectContent,
@@ -6,53 +8,166 @@ import {
   SelectValue,
 } from "@/app/(client)/_components/ui/select";
 
+import { useQueryState } from "next-usequerystate";
+
 const Filters = () => {
+  const [category, setCategory] = useQueryState("category");
+  const [rating, setRating] = useQueryState("rating");
+  const [offers, setOffers] = useQueryState("offers");
+  const [sortby, setSortby] = useQueryState("sortby");
+
+  const filtersData = [
+    {
+      title: "Category",
+      position: "left",
+      getter: category,
+      setter: setCategory,
+      values: [
+        {
+          value: "mobiles",
+          placeholder: "Mobiles",
+        },
+        {
+          value: "headphones",
+          placeholder: "Headphones",
+        },
+        {
+          value: "laptops",
+          placeholder: "Laptops",
+        },
+        {
+          value: "watches",
+          placeholder: "Watches",
+        },
+        {
+          value: "clear",
+          placeholder: "Clear",
+        },
+      ],
+    },
+    {
+      title: "Rating",
+      position: "left",
+      getter: rating,
+      setter: setRating,
+      values: [
+        {
+          value: "1",
+          placeholder: "1+",
+        },
+        {
+          value: "2",
+          placeholder: "2+",
+        },
+        {
+          value: "3",
+          placeholder: "3+",
+        },
+        {
+          value: "4",
+          placeholder: "4+",
+        },
+        {
+          value: "clear",
+          placeholder: "Clear",
+        },
+      ],
+    },
+    {
+      title: "Offers",
+      position: "left",
+      getter: offers,
+      setter: setOffers,
+      values: [
+        {
+          value: "30",
+          placeholder: "30% off & above",
+        },
+        {
+          value: "60",
+          placeholder: "60% off & above",
+        },
+        {
+          value: "clear",
+          placeholder: "Clear",
+        },
+      ],
+    },
+    {
+      title: "Sort by",
+      position: "right",
+      getter: sortby,
+      setter: setSortby,
+      values: [
+        {
+          value: "PLH",
+          placeholder: "Price - Low to High",
+        },
+        {
+          value: "PHL",
+          placeholder: "Price - High to Low",
+        },
+        {
+          value: "clear",
+          placeholder: "Clear",
+        },
+      ],
+    },
+  ];
+
+  const handleChange = (value: string, setter: any) => {
+    !value || value === "clear" ? setter(null) : setter(value);
+  };
+
   return (
     <div className="flex justify-between">
       <div className="flex gap-8">
-        <Select>
-          <SelectTrigger className="w-[120px] rounded-full font-semibold bg-slate-200">
-            <SelectValue placeholder="Category" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="mobiles">Mobiles</SelectItem>
-            <SelectItem value="headphones">Headphones</SelectItem>
-            <SelectItem value="laptops">Laptops</SelectItem>
-            <SelectItem value="watches">Watches</SelectItem>
-          </SelectContent>
-        </Select>
-        <Select>
-          <SelectTrigger className="w-[120px] rounded-full font-semibold bg-slate-200">
-            <SelectValue placeholder="Rating" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="1">1+</SelectItem>
-            <SelectItem value="2">2+</SelectItem>
-            <SelectItem value="3">3+</SelectItem>
-            <SelectItem value="4">4+</SelectItem>
-          </SelectContent>
-        </Select>
-        <Select>
-          <SelectTrigger className="w-[120px] rounded-full font-semibold bg-slate-200">
-            <SelectValue placeholder="Offers" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="30%">Min. 30% discount</SelectItem>
-            <SelectItem value="60%">Min. 60% discount</SelectItem>
-          </SelectContent>
-        </Select>
+        {filtersData.map((item, idx) => {
+          if (item.position !== "left") return;
+
+          return (
+            <Select
+              key={idx}
+              onValueChange={(value) => handleChange(value, item.setter)}
+              value={item.getter || undefined}
+            >
+              <SelectTrigger className="rounded-full font-semibold bg-slate-200">
+                <SelectValue placeholder={item.title} />
+              </SelectTrigger>
+              <SelectContent>
+                {item.values.map((kv, idx) => (
+                  <SelectItem key={idx} value={kv.value}>
+                    {kv.placeholder}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          );
+        })}
       </div>
       <div>
-        <Select>
-          <SelectTrigger className="w-[120px] rounded-full font-semibold">
-            <SelectValue placeholder="Sort by" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="light">Price - Low to High</SelectItem>
-            <SelectItem value="dark">Price - High to Low</SelectItem>
-            <SelectItem value="system">Rating - High to Low</SelectItem>
-          </SelectContent>
-        </Select>
+        {filtersData.map((item, idx) => {
+          if (item.position !== "right") return;
+
+          return (
+            <Select
+              key={idx}
+              onValueChange={(value) => handleChange(value, item.setter)}
+              value={item.getter || undefined}
+            >
+              <SelectTrigger className="rounded-full font-semibold">
+                <SelectValue placeholder={item.title} />
+              </SelectTrigger>
+              <SelectContent>
+                {item.values.map((kv, idx) => (
+                  <SelectItem key={idx} value={kv.value}>
+                    {kv.placeholder}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          );
+        })}
       </div>
     </div>
   );
