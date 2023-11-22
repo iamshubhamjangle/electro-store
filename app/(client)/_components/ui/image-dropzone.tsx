f2c46438755a48cd1cf215a38195789c9bd4e434
Shubham Jangle <jangleshubham13@gmail.com>
2023-11-22 15:14:18 +0530

    feat: Dropzone Improvement

--- app/(client)/_components/ui/image-dropzone.tsx
+++ app/(client)/_components/ui/image-dropzone.tsx
@@ -64,7 +64,8 @@ const useImageDropzone = (multiple: boolean): ImageDropzoneHook => {
         >
           <input {...getInputProps()} />
           <p className="text-sm text-muted-foreground">
-            Drag and drop dropzoneImages here, or click to select dropzoneImages
+            Drag and drop {multiple ? "images" : "image"} here, or click to
+            select dropzoneImages
           </p>
         </div>
         <div className="p-4 space-y-4">
