cd88e4f00ca4e255ee30db949be5a18ebd7b0144
Shubham Jangle <jangleshubham13@gmail.com>
2023-12-02 13:01:13 +0530

    feat: Order api

--- app/(server)/api/order/route.ts
+++ app/(server)/api/order/route.ts
@@ -10,50 +10,83 @@ export async function POST(req: NextRequest) {
     if (!session) return new NextResponse("Unauthorized", { status: 401 });
     const userId = session.user?.id;
 
-    const cartItems = await prisma.cart.findMany({
+    const cartItems = await prisma.cartProduct.findMany({
       where: {
         userId,
       },
     });
 
-    // let orderTotal = cartItems.reduce(
-    //   (total, product) => total + product.product_current_price,
-    //   0
-    // );
+    let orderTotal = cartItems.reduce(
+      (total, product) =>
+        (total + product.product_current_price) * product.product_quantity,
+      0
+    );
 
-    // // Create a new order
-    // const order = await prisma.order.create({
-    //   data: {
-    //     userId: userId,
-    //     orderTotal,
-    //     products: {
-    //       createMany: {
-    //         data: cartItems?.map((cartItem) => {
-    //           return {
-    //             product_id: cartItem.product_id,
-    //             product_title: cartItem.product_title,
-    //             product_sub_title: cartItem.product_sub_title,
-    //             product_quantity: cartItem.product_quantity,
-    //             product_current_price: cartItem.product_current_price,
-    //             product_original_price: cartItem.product_original_price,
-    //           };
-    //         }),
-    //       },
-    //     },
-    //   },
-    //   select: {
-    //     id: true,
-    //   },
-    // });
+    const indianCities = [
+      "Mumbai",
+      "Delhi",
+      "Bangalore",
+      "Hyderabad",
+      "Ahmedabad",
+      "Chennai",
+      "Kolkata",
+      "Surat",
+      "Pune",
+      "Jaipur",
+      "Lucknow",
+      "Kanpur",
+      "Nagpur",
+      "Visakhapatnam",
+      "Indore",
+      "Thane",
+      "Bhopal",
+      "Patna",
+      "Vadodara",
+      "Ghaziabad",
+      "Ludhiana",
+      "Agra",
+      "Nashik",
+      "Faridabad",
+      "Meerut",
+      "Rajkot",
+      "Varanasi",
+      "Srinagar",
+      "Aurangabad",
+    ];
 
-    // // Delete the cart items after placing the order
-    // await prisma.cartItem.deleteMany({
-    //   where: {
-    //     userId: userId,
-    //   },
-    // });
+    // Create a new order
+    await prisma.order.create({
+      data: {
+        userId,
+        orderTotal,
+        address: indianCities[Math.floor(Math.random() * indianCities.length)],
+        products: {
+          createMany: {
+            data: cartItems?.map((cartItem) => {
+              return {
+                product_id: cartItem.product_id,
+                product_title: cartItem.product_title,
+                product_sub_title: cartItem.product_sub_title,
+                product_quantity: cartItem.product_quantity,
+                product_current_price: cartItem.product_current_price,
+                product_original_price: cartItem.product_original_price,
+              };
+            }),
+          },
+        },
+      },
+    });
 
-    // return NextResponse.json({ orderId: order.id }, { status: 200 });
+    await prisma.cartProduct.deleteMany({
+      where: {
+        userId,
+      },
+    });
+
+    return NextResponse.json(
+      { message: "Order placed successfully" },
+      { status: 200 }
+    );
   } catch (error) {
     console.error("<<< ERROR >>><<< API/ORDER/POST >>>");
     console.error(error);
